bee22e474b8f6997420a3832221fd1bb6fa6247b
choi-jihoon <jihoonchoi.94@gmail.com>
2022-03-22 20:46:21 -0700

    Notify functionality on click

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -8,7 +8,8 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
 
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
